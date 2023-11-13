973baa56b35544e220676ba2e91da2e7c82d53a8
mvijay2 <vijjuvolley1@gmail.com>
2023-11-13 18:07:55 +0530

    added local changes

---
+++ weather_pred.py
@@ -0,0 +1,41 @@
+
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load the saved model
+loaded_model = joblib.load("D:\git projects\weather_prediction\weatherpred.joblib")
+
+# Streamlit app
+st.title('weather prediction')
+
+def weather_prediction(input_data):
+   
+    np_array=np.asarray(input_data)
+    data=np_array.reshape(1,-1)
+    #data=scaler.fit_transform(reshaped)
+    prediction = loaded_model.predict(data)
+    #prediction
+    if prediction==0:
+        return 'NO RAIN, lets dry whatever you want'
+    else:
+        return 'Raining'
+                       
+def main():
+    
+    day=st.text_input('enter day')
+    month=st.text_input('enter month')
+    year=st.text_input('enter year')
+    TMAX=st.text_input('enter t_max')
+    TMIN=st.text_input('enter t_min')
+    
+    result=''
+
+    if st.button('predict weather'):
+
+      result=weather_prediction([day,month,year,TMAX,TMIN])
+
+    st.success(result)
+
+main()
+   
